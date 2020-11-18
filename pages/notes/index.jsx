bc705c78f78937075f60dd333a1fa7727ca49f9c
Baheya <khalifa.baheya@gmail.com>
2020-11-18 18:13:11 +0400

    getting server side props from notes local api

--- pages/notes/index.jsx
+++ pages/notes/index.jsx
@@ -3,11 +3,7 @@
 import { jsx } from 'theme-ui';
 import Link from 'next/link';
 
-const Notes = () => {
-  const notes = new Array(15)
-    .fill(1)
-    .map((e, i) => ({ id: i, title: `This is my note ${i}` }));
-
+const Notes = ({ notes }) => {
   return (
     <div sx={{ variant: 'containers.page' }}>
       <h1>My Notes</h1>
@@ -21,7 +17,7 @@ const Notes = () => {
         }}
       >
         {notes.map((note) => (
-          <div sx={{ width: '33%', p: 2 }}>
+          <div sx={{ width: '33%', p: 2 }} key={note.id}>
             <Link key={note.id} href="/notes/[id]" as={`/notes/${note.id}`}>
               <a sx={{ textDecoration: 'none', cursor: 'pointer' }}>
                 <div sx={{ variant: 'containers.card' }}>
@@ -35,5 +31,13 @@ const Notes = () => {
     </div>
   );
 };
+
+export async function getServerSideProps() {
+  const res = await fetch(`http://localhost:3000/api/note`);
+  const { data } = await res.json();
+  return {
+    props: { notes: data },
+  };
+}
 
 export default Notes;
